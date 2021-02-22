38286dc3b6b15871ab6e1e46d67044f6ff66be08
huw <huw@hey.com>
2021-02-22 13:53:14 +1100

    Set Atlassian npm tokens where available

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 cp -r .config .zshrc $HOME/
 curl -L https://git.io/antigen > $HOME/.antigen.zsh
 curl -fsSL https://starship.rs/install.sh | bash -s -- --yes
+
+# Set up Yarn tokens (if available)
+if [ -n ${NPM_AUTH_TOKEN_ATLASSIAN+x} ]; 
+then yarn config set --home npmScopes.atlassian.npmAuthToken $NPM_AUTH_TOKEN_ATLASSIAN 
+fi
+
+if [ -n ${NPM_AUTH_TOKEN_ATLASSIANSOX+x} ]; 
+then yarn config set --home npmScopes.atlassiansox.npmAuthToken $NPM_AUTH_TOKEN_ATLASSIANSOX 
+fi
