dcc0f682ccf8a16474d630e45e41a5249352b1b9
huw <huw@hey.com>
2021-02-22 14:11:52 +1100

    Add SSH key logic and update conditionals

--- setup.sh
+++ setup.sh
@@ -3,10 +3,19 @@ curl -L https://git.io/antigen > $HOME/.antigen.zsh
 curl -fsSL https://starship.rs/install.sh | bash -s -- --yes
 
 # Set up Yarn tokens (if available)
-if [ -n ${NPM_AUTH_TOKEN_ATLASSIAN+x} ]; 
-then yarn config set --home npmScopes.atlassian.npmAuthToken $NPM_AUTH_TOKEN_ATLASSIAN 
+if [ ! -z ${NPM_AUTH_TOKEN_ATLASSIAN+x} ]; 
+then 
+  yarn config set --home npmScopes.atlassian.npmAuthToken $NPM_AUTH_TOKEN_ATLASSIAN 
 fi
 
-if [ -n ${NPM_AUTH_TOKEN_ATLASSIANSOX+x} ]; 
-then yarn config set --home npmScopes.atlassiansox.npmAuthToken $NPM_AUTH_TOKEN_ATLASSIANSOX 
+if [ ! -z ${NPM_AUTH_TOKEN_ATLASSIANSOX+x} ]; 
+then 
+  yarn config set --home npmScopes.atlassiansox.npmAuthToken $NPM_AUTH_TOKEN_ATLASSIANSOX 
+fi
+
+# Set up SSH key for Codespaces
+if [ ! -z ${CODESPACES_SSH_KEY_PRIVATE+x} ]; 
+then 
+  echo $CODESPACES_SSH_KEY_PRIVATE > ~/.ssh/id_codespaces
+  chmod 600 ~/.ssh/id_codespaces
 fi
